ff27f0991eb72ba2845597a90bd27fde58984bd6
julio sandoval <ingenierojuliosandoval@outlook.com>
2022-09-26 10:01:41 -0500

    first commit

---
+++ src/main/java/com/finappers/App.java
@@ -0,0 +1,13 @@
+package com.finappers;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
